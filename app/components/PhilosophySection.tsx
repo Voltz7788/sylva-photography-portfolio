c5694917e9c5e21c0167c9e2a18d29650f8786a5
Yannis <yannismadu777@gmail.com>
2023-09-29 22:29:41 +0100

    Add some responsivity to philosophy section

--- app/components/PhilosophySection.tsx
+++ app/components/PhilosophySection.tsx
@@ -18,18 +18,22 @@ export default function PhilosophySection() {
           alt=""
           className="object-cover w-full h-full"
         />
-        <div className="row-span-2 flex flex-col justify-between gap-3">
-          <Image src={MusicLandscape1} alt="" />
-          <div className="text-center w-3/5 m-auto">
-            <h2 className="text-3xl font-bold uppercase">My Philosophy</h2>
-            <p className="text-xl">The best wedding photography is candid.</p>
-            <p className="mt-7">
+        <div className="row-span-2 flex flex-col justify-between gap-3 h-full">
+          <Image src={MusicLandscape1} alt="" className="object-cover w-full" />
+          <div className="text-center w-4/5 lg:w-4/5 xl:w-3/5 m-auto">
+            <h2 className="text-2xl md:text-3xl font-bold uppercase">
+              My Philosophy
+            </h2>
+            <p className="text-base md:text-lg lg:text-xl mt-2">
+              The best wedding photography is candid.
+            </p>
+            <p className="mt-7 text-sm md:text-md lg:text-base">
               {`It's when you're being yourself and forget all about the camera.
               It's all of those inbetween moments. Not Planned. Not Posed. Just
               real moments captured in the most beautiful way possible.`}
             </p>
           </div>
-          <Image src={MusicLandscape2} alt="" />
+          <Image src={MusicLandscape2} alt="" className="object-cover w-full" />
         </div>
         <Image
           src={MusicPortrait3}
